56ba3462ae0fd0d0f02765d9772d8eb0209c32f2
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2012-04-26 19:22:08 -0400

    Add more methods

--- lib/SDL.pm
+++ lib/SDL.pm
@@ -8,12 +8,42 @@ class SDL {
         is native('libSDL')
         { * }
 
+    my sub SDL_InitSubSystem(int32 $flags)
+        returns int32
+        is native('libSDL')
+        { * }
+
+    my sub SDL_QuitSubSystem(int32 $flags)
+	is native('libSDL')
+        { * }
+
     my sub SDL_Quit()
 	is native('libSDL')
         { * }
 
+    my sub SDL_WasInit(int32 $flags)
+        returns int32
+        is native('libSDL')
+        { * }
+
+    my sub SDL_GetError()
+        returns Str
+        is native('libSDL')
+        { * }
+
     method init(int32 $flags) returns int32 { SDL_Init($flags) }
+
+    method init_sub_system(int32 $flags) returns int32 {
+        SDL_InitSubSystem($flags)
+    }
+
+    method quit_sub_system(int32 $flags) { SDL_QuitSubSystem($flags) }
+
     method quit() { SDL_Quit() }
+
+    method was_init(int32 $flags) returns int32 { SDL_WasInit($flags) }
+
+    method get_error() returns Str { SDL_GetError() }
 }
 
 # vim: ft=perl6
